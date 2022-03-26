add85edcde4c83901bc8fc71452130c6b7af928d
sabarish2494 <sabarivasudevan@gmail.com>
2022-03-26 17:03:57 +0530

    first update

---
+++ src/org/gitup/GitupSample.java
@@ -0,0 +1,10 @@
+package org.gitup;
+
+public class GitupSample {
+	public static void employee() {
+System.out.println("employee name is sabarish");
+	}
+public static void main(String[] args) {
+	employee();
+}
+}
